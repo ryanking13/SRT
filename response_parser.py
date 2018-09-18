8dfeec05c7a04c42bdefeb7885b2f8afa67fa3db
ryanking13 <def6488@gmail.com>
2018-09-18 14:17:30 +0900

    add SRTResponseData class

--- response_parser.py
+++ response_parser.py
@@ -5,6 +5,63 @@ DATASETTAG = NAMESPACE + 'Dataset'
 DATATAG = NAMESPACE + 'Col'
 
 
+class SRTResponseData():
+    NAMESPACE = '{http://www.nexacro.com/platform/dataset}'
+    DATASETTAG = NAMESPACE + 'Dataset'
+    DATATAG = NAMESPACE + 'Col'
+
+    STATUS_OUTPUT_ID = 'dsOutput0'
+    DATA_OUTPUT_ID = 'dsCmcOutput0'
+
+    STATUS_SUCCESS = 'SUCC'
+    STATUS_FAIL = 'FAIL'
+
+    def __init__(self, response, auto_parse=True):
+        self._xml = ET.fromstring(response)
+        self._status = {}
+        self._data = {}
+
+        self._parsed = False
+        if auto_parse:
+            self._parse()
+
+    def __str__(self):
+        return self.dump()
+
+    def dump(self):
+        return self.XML_PREFIX + ET.tostring(self._xml, encoding='unicode')
+
+    def _parse(self):
+        datasets = filter(lambda e: e.tag == DATASETTAG, list(tree))
+        for dataset in datasets:
+            tag_id = dataset.get('id')
+
+            # status check
+            if tag_id == self.STATUS_OUTPUT_ID:
+                for row in dataset.iter(DATATAG):
+                    self._status[row.get('id')] = row.text
+
+            # data check
+            elif tag_id == self.DATA_OUTPUT_ID:
+                for row in dataset.iter(DATATAG):
+                    self._data[row.get('id')] = row.text
+
+        self._parsed = True
+
+    def success(self):
+        result = status.get('strResult', None)
+        if result is None:
+            # TODO: raise error
+            return False
+        if result == self.STATUS_SUCCESS:
+            return True
+        elif result == self.STATUS_FAIL:
+            return False
+        else:
+            # TODO: raise error
+            return False
+
+
 def parse_login(response):
     tree = ET.fromstring(response)
     datasets = filter(lambda e: e.tag == DATASETTAG, list(tree))
