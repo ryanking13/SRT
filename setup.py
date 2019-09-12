d4590c5c33c8c48ea1677c3cabb22e0f25eb4186
def6488@gmail.com <def6488@gmail.com>
2019-09-13 00:06:53 +0900

    release v1.0.0

--- setup.py
+++ setup.py
@@ -2,27 +2,26 @@ from setuptools import find_packages, setup
 
 
 def long_descrtiption():
-    with open('README.md', 'r', encoding='utf-8') as f:
+    with open("README.md", "r", encoding="utf-8") as f:
         desc = f.read()
     return desc
 
 
-setup(name='SRTrain',
-      author='ryanking13',
-      author_email='def6488@gmail.com',
-      version='0.1.6',
-      description='SRT(Super Rapid Train) wrapper for python',
-      long_description=long_descrtiption(),
-      long_description_content_type='text/markdown',
-      license='MIT',
-      url='https://github.com/ryanking13/SRT',
-      packages=find_packages(),
-      install_requires=[
-          'requests'
-      ],
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'Environment :: Console',
-          'License :: OSI Approved :: MIT License',
-      ]
-      )
+setup(
+    name="SRTrain",
+    author="ryanking13",
+    author_email="def6488@gmail.com",
+    version="1.0.0",
+    description="SRT(Super Rapid Train) wrapper for python",
+    long_description=long_descrtiption(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/ryanking13/SRT",
+    packages=find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
