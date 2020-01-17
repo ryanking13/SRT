9804243c15655199a591d4f1bd45b3b4c78bfb8e
sihadan <def6488@gmail.com>
2020-01-17 14:47:25 +0900

    bump version to v1.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="SRTrain",
     author="ryanking13",
     author_email="def6488@gmail.com",
-    version="1.0.2",
+    version="1.0.3",
     description="SRT(Super Rapid Train) wrapper for python",
     long_description=long_descrtiption(),
     long_description_content_type="text/markdown",
