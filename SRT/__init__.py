b6660630b6ba81c03e8782f6d7b851b04babb729
Gyeongjae Choi <def6488@gmail.com>
2022-10-31 15:33:58 +0900

    2.0.4 (#196)

--- SRT/__init__.py
+++ SRT/__init__.py
@@ -2,7 +2,7 @@ from .errors import SRTError, SRTLoginError, SRTNotLoggedInError, SRTResponseErr
 from .passenger import Adult, Child, Disability1To3, Disability4To6, Passenger, Senior
 from .srt import SRT
 
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 
 __all__ = [
     "SRT",
