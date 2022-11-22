450367bc2a1d2213fdb5ed17b39dda04be5226f0
Kangwhi Kim <kkh8123@naver.com>
2022-11-23 00:07:11 +0900

    feat: 필요없는 파일 제거

--- test.py
+++
@@ -1,6 +0,0 @@
-from SRT import SRT, SeatType
-
-session = SRT('', '')
-trains = session.search_train('수서', '부산', '20221122', '060000')
-reserve = session.reserve(trains[0], special_seat=SeatType.SPECIAL_FIRST)
-''
