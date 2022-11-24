081ce67cbe64a80951b963a1f5c6221d6d24cf33
Gyeongjae Choi <def6488@gmail.com>
2022-11-24 17:26:01 +0900

    2.0.5 (#203)

--- SRT/__init__.py
+++ SRT/__init__.py
@@ -3,7 +3,7 @@ from .passenger import Adult, Child, Disability1To3, Disability4To6, Passenger, 
 from .seat_type import SeatType
 from .srt import SRT
 
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 
 __all__ = [
     "SRT",
