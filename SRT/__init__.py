fca903b73ed2a599f81e83c49efaf607a1b3a974
ryanking13 <def6488@gmail.com>
2023-08-02 21:51:15 +0900

    Use hatchling

--- SRT/__init__.py
+++ SRT/__init__.py
@@ -3,8 +3,6 @@ from .passenger import Adult, Child, Disability1To3, Disability4To6, Passenger, 
 from .seat_type import SeatType
 from .srt import SRT
 
-__version__ = "2.0.8"
-
 __all__ = [
     "SRT",
     "SRTError",
