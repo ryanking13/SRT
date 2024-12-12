2373848e0bb7d5c861d4e8f0cb3d6996db4a4869
Monsteel <dev.e0eun@gmail.com>
2024-12-12 09:23:33 +0900

    Add netfunnel test

---
+++ tests/test_netfunnel.py
@@ -0,0 +1,23 @@
+import pytest
+
+from SRT.netfunnel import NetFunnelHelper, SRTNetFunnelError
+
+@pytest.fixture(scope="module")
+def helper():
+    return NetFunnelHelper()
+
+def test_get_netfunnel_key_success(helper):
+    try:
+        key = helper.get_netfunnel_key(True)
+    except SRTNetFunnelError as e:
+        assert False
+    assert key is not None
+
+
+def test_set_complete_success(helper):
+    key = helper.get_netfunnel_key(True)
+    try:
+        helper.set_complete(key)
+    except SRTNetFunnelError as e:
+        assert False
+    assert True
