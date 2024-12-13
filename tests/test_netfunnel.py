2ca74614504b5583c6daa97a8a7a1f90cf26f72b
ryanking13 <def6488@gmail.com>
2024-12-13 23:27:55 +0900

    remove fixture

--- tests/test_netfunnel.py
+++ tests/test_netfunnel.py
@@ -1,12 +1,12 @@
 from SRT.netfunnel import NetFunnelHelper, NetFunnelResponse
 
 
-def test_get_netfunnel_key_success(helper):
+def test_get_netfunnel_key_success():
     helper = NetFunnelHelper()
     helper._get_netfunnel_key()
 
 
-def test_set_complete_success(helper):
+def test_set_complete_success():
     helper = NetFunnelHelper()
     key = helper._get_netfunnel_key()
     helper._set_complete(key)
